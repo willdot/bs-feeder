0b23e3e00c6c0c723d347e3f55f9754ae36bd288
Will Andrews <will7989@hotmail.com>
2024-11-02 20:46:01 +0000

    add the files

---
+++ consumer.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"log/slog"
+	"time"
+
+	apibsky "github.com/bluesky-social/indigo/api/bsky"
+	"github.com/bluesky-social/jetstream/pkg/client"
+	"github.com/bluesky-social/jetstream/pkg/client/schedulers/sequential"
+	"github.com/bluesky-social/jetstream/pkg/models"
+)
+
+type consumer struct {
+	cfg *client.ClientConfig
+}
+
+func NewConsumer(jsAddr string) *consumer {
+	cfg := client.DefaultClientConfig()
+	if jsAddr != "" {
+		cfg.WebsocketURL = jsAddr
+	}
+	cfg.WantedCollections = []string{
+		"app.bsky.feed.post",
+	}
+	return &consumer{
+		cfg: cfg,
+	}
+}
+
+func (con *consumer) Consume(ctx context.Context, logger *slog.Logger) error {
+	h := &handler{
+		seenSeqs: make(map[int64]struct{}),
+	}
+
+	scheduler := sequential.NewScheduler("jetstream_localdev", logger, h.HandleEvent)
+
+	// TODO: logger
+	c, err := client.NewClient(con.cfg, slog.Default(), scheduler)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+
+	cursor := time.Now().Add(5 * -time.Minute).UnixMicro()
+
+	// Every 5 seconds print the events read and bytes read and average event size
+	go func() {
+		ticker := time.NewTicker(5 * time.Second)
+		for {
+			select {
+			case <-ticker.C:
+				eventsRead := c.EventsRead.Load()
+				bytesRead := c.BytesRead.Load()
+				avgEventSize := bytesRead / eventsRead
+				logger.Info("stats", "events_read", eventsRead, "bytes_read", bytesRead, "avg_event_size", avgEventSize)
+			}
+		}
+	}()
+
+	if err := c.ConnectAndRead(ctx, &cursor); err != nil {
+		return fmt.Errorf("connect and read: %w", err)
+	}
+
+	slog.Info("stopping consume")
+	return nil
+}
+
+type handler struct {
+	seenSeqs  map[int64]struct{}
+	highwater int64
+}
+
+func (h *handler) HandleEvent(ctx context.Context, event *models.Event) error {
+	// Unmarshal the record if there is one
+	if event.Commit != nil && (event.Commit.Operation == models.CommitOperationCreate || event.Commit.Operation == models.CommitOperationUpdate) {
+		switch event.Commit.Collection {
+		case "app.bsky.feed.post":
+			var post apibsky.FeedPost
+			if err := json.Unmarshal(event.Commit.Record, &post); err != nil {
+				return fmt.Errorf("failed to unmarshal post: %w", err)
+			}
+
+			if event.Account.Did == "did:plc:dadhhalkfcq3gucaq25hjqon" {
+				fmt.Printf("%v |(%s)| %s\n", time.UnixMicro(event.TimeUS).Local().Format("15:04:05"), event.Did, post.Text)
+			}
+		}
+	}
+
+	return nil
+}
