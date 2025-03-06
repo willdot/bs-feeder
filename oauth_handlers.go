2b11ea1df29b3a0642944c4819ed3ccba62d68df
Will Andrews <will7989@hotmail.com>
2025-03-06 20:02:15 +0000

    try this redirect

--- oauth_handlers.go
+++ oauth_handlers.go
@@ -137,7 +137,7 @@ func (s *Server) HandleLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Add("HX-Redirect", u.String())
+	w.Header().Add("HX-Redirect", "/")
 	http.Redirect(w, r, u.String(), http.StatusOK)
 }
 
@@ -241,6 +241,7 @@ func (s *Server) handleOauthCallback(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Add("HX-Redirect", "/bookmarks")
 	http.Redirect(w, r, "/bookmarks", http.StatusOK)
 }
 
